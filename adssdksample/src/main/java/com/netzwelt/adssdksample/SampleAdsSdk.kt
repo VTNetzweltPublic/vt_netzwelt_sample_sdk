80430a4d9a5db75a37a964f111b32becbf8f4298
Manish Patiyal <manish.patiyal.netzwelt@gmail.com>
2019-11-11 17:37:50 +0530

    Change of Display Message and version update to 1.1

--- adssdksample/src/main/java/com/netzwelt/adssdksample/SampleAdsSdk.kt
+++ adssdksample/src/main/java/com/netzwelt/adssdksample/SampleAdsSdk.kt
@@ -1,7 +1,7 @@
 package com.netzwelt.adssdksample
 
 object SampleAdsSdk {
-    fun showDemo(): String {
-        return "Release 1.0 of Sample SDK working"
+    fun showWelcomeMessage(): String {
+        return "Release 1.1 of Sample SDK Test"
     }
 }
