64e1856e2515cc21b401dfc6d1e6f36f4dfeb929
Manish Patiyal <manish.patiyal.netzwelt@gmail.com>
2019-11-11 17:28:42 +0530

    Change of Display Message

--- adssdksample/src/main/java/com/netzwelt/adssdksample/SampleAdsSdk.kt
+++ adssdksample/src/main/java/com/netzwelt/adssdksample/SampleAdsSdk.kt
@@ -2,6 +2,6 @@ package com.netzwelt.adssdksample
 
 object SampleAdsSdk {
     fun showDemo(): String {
-        return "Text From Sample SDK"
+        return "Release 1.0 of Sample SDK working"
     }
 }
